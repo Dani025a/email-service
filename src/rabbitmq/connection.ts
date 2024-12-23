0fa984b2163c47f2e3fed42cb72d0b1cbb71ad87
Dani025a <dani025a@edu.easj.dk>
2024-12-23 16:37:01 +0100

    Small changes

--- src/rabbitmq/connection.ts
+++ src/rabbitmq/connection.ts
@@ -5,7 +5,7 @@ let connection: amqp.Connection | null = null;
 export const getRabbitMQConnection = async (): Promise<amqp.Connection> => {
   try {
     if (!connection) {
-      connection = await amqp.connect(process.env.RABBITMQ_URL || 'amqp://localhost');
+      connection = await amqp.connect(process.env.RABBITMQ_URL || 'amqp://guest:guest@rabbitmq:5672');
     }
     return connection;
   } catch (error) {
